429aee41b915ed470dcafd515b1068fb084ede9b
Joshua Lewis <lewisjoshua1986@gmail.com>
2026-02-28 16:50:21 -0600

    chore(errors): register global error interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
+
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,6 +9,11 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
+    },
     provideRouter(routes)
   ]
 };
