ae40621f521ba5c9ffe721cf11803080cf16182d
Joshua Lewis <lewisjoshua1986@gmail.com>
2026-03-01 09:38:13 -0600

    feat(env): add environment-based API switching

--- src/app/core/api/implementations/task-api.http.ts
+++ src/app/core/api/implementations/task-api.http.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { environment } from "../../../../../environments/environment";
 import { TaskApi } from "../interfaces/task-api.interface";
+import { environment } from "../../../../environments/environment";
+
 
 @Injectable()
 export class TaskApiHttp implements TaskApi {
